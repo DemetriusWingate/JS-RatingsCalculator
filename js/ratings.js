d62b03b7f3160af2387c0dcd88c12a9fbe8e704e
Demetrius Wingate <dwingate1991@gmail.com>
2021-11-14 20:21:08 -0500

    Completed module

---
+++ js/ratings.js
@@ -0,0 +1,24 @@
+function collect_ratings() {
+    let ratings = {
+        count:0,
+        sum: 0,
+        average: 0
+    };
+
+    let rating = 0;
+    const elements = document.querySelectorAll('.rating');
+
+    elements.forEach( element => {
+        rating = parseInt(element.id.replace('star', ''));
+        ratings.count += parseInt(element.value);
+        ratings.sum += rating * parseInt(element.value);
+    })
+
+    if (ratings.count !== 0) {
+        ratings.average = ratings.sum / ratings.count;
+    }
+
+    return ratings;
+
+};
+
